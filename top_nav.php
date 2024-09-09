00c09a3727314ad9f28f5fdfdac773dc62af34e4
Himan Manduja <hghimanmanduja@gmail.com>
2024-09-09 08:34:57 +0530

    notification system adjustment

--- top_nav.php
+++ top_nav.php
@@ -17,6 +17,9 @@
           <a class="nav-link" href="display_content.php">Information</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="recommendations.php">Get help</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
