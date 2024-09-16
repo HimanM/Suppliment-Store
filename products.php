492424a408d431b3d531b249b6f00293f8dec0c4
Nimesh357 <nimeshbala357@gmail.com>
2024-09-16 19:45:04 +0530

    changes in product bg and nav css

--- products.php
+++ products.php
@@ -12,6 +12,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="CSS/login_styles.css">
     <script src="JS/products.js" defer></script>
+    <style>
+        body {
+            background-image: url("./images/bg2.png");
+            background-size: cover;
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            
+        }
+        </style>
 </head>
 <body>
 <?php include 'top_nav.php'; ?>
