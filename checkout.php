7ab8d340a9248aae22957b49e81971d7111a47b4
HimanM <67066047+HimanM@users.noreply.github.com>
2024-09-24 03:37:44 +0530

    added styling options(not implemented yet)

--- checkout.php
+++ checkout.php
@@ -52,158 +52,158 @@ while ($row = $result->fetch_assoc()) {
       <div class="row">
 
         <div class="col-md-4 order-2">
-          
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span class=text-muted>Your Cart</span>
             <span class="badge rounded-pill bg-secondary"><?php echo count($cart_items); ?></span>
           </h4>
           
-          <div class="card" style="">
-            <?php foreach ($cart_items as $item): ?>
-              <li class="list-group-item d-flex justify-content-between lh-condensed">
-                <div>
-                  <h6 class="my-1 mx-1"><?php echo $item['name']; ?></h6>
-                  <small class="text-muted mx-1">Quantity: <?php echo $item['quantity']; ?></small>
-                </div>
-                <span class="text-muted my-1 mx-1">Price: $<?php echo number_format($item['price'], 2); ?></span>
-              </li>
-            <?php endforeach; ?>
-          </div>
+          <div class="container mb-4 p-4 glass-card">
+            <div class="card" style="">
+              <?php foreach ($cart_items as $item): ?>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-1 mx-1"><?php echo $item['name']; ?></h6>
+                    <small class="text-muted mx-1">Quantity: <?php echo $item['quantity']; ?></small>
+                  </div>
+                  <span class="text-muted my-1 mx-1">Price: $<?php echo number_format($item['price'], 2); ?></span>
+                </li>
+              <?php endforeach; ?>
+            </div>
 
-          <div class="card-footer d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong><?php echo number_format($total, 2); ?></strong>
+            <div class="card-footer d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong><?php echo number_format($total, 2); ?></strong>
+            </div>
           </div>
-
         </div>
          
         <form class="col-md-8 order-1" action="PHP/process_checkout.php" method="POST">
-          <h4 class="mb-3">Billing Address</h4>
-          <div class="row">
-            <div class="col mb-4">
-              <label for="First name"> First Name </label>
-              <input type="text" class="form-control" placeholder="First name" aria-label="First name"  required>
-            </div>
-            <div class="col mb-4">
-              <label for="La\st name"> Last Name </label>
-              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"  required>
-            </div>
-          </div>
-          
-          <div class="mb-4">
-            <label for="email">Email (optional)</label>
-            <input type="text" class="form-control" placeholder="you@example.com" aria-label="email">
-          </div>
-          
-          <div class="mb-4">
-            <label for="Address">Address</label>
-            <input type="text" class="form-control" placeholder="1234 Main St" aria-label="Address" id="shipping_address" name="shipping_address"  required>
-          </div>
-          
-          <div class="mb-4">
-            <label for="Address2">Address 2 (optional)</label>
-            <input type="text" class="form-control" placeholder="Appartment or suite" aria-label="Address2">
-          </div>
-          
-          <div class="row">
-             <div class="col">
-              <label for="country">Country</label>
-              <select class="form-select">
-                <option selected>Choose...</option>
-                <option value="1">Sri Lanka</option>
-                <option value="2">Japan</option>
-                <option value="3">Australlia</option>
-            </select>
-            </div>
-             <div class="col">
-              <label for="state">State</label>
-              <select class="form-select">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
-            </div>
-             <div class="col mb-4">
-              <label for="zip">Zip Code</label>
-              <input type="text" class="form-control" aria-label="zip">
-            </div>
-            
-            <hr class="mb-4">
-            
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-              <label class="form-check-label" for="flexCheckDefault">
-                Shipping address is the same as my billing address
-              </label>
-            </div>
-            
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-              <label class="form-check-label" for="flexCheckChecked">
-                Save this information for next time
-              </label>
-            </div>
-            
-            <hr class="mb-4">
-            
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
-              <label class="form-check-label" for="flexRadioDefault1">
-                Credit card (Default)
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
-              <label class="form-check-label" for="flexRadioDefault2">
-                Debit card
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
-              <label class="form-check-label" for="flexRadioDefault3">
-                Paypal
-              </label>
-            </div>
-            
-            <div class="row">
+        <h4 class="mb-3">Billing Address</h4>
+          <div class="container mb-4 p-4 glass-card">
+            <div class ="row">
               <div class="col mb-4">
-                <label for="Card1">
-                  Name on card
-                </label>
-                <input type="text" class="form-control"aria-label="card1" required>
-                <small class="text-muted">
-                  Full name, as displayed on the card
-                </small>
+                <label for="First name"> First Name </label>
+                <input type="text" class="form-control" placeholder="First name" aria-label="First name"  required>
+              </div>
+              <div class="col mb-4">
+                <label for="La\st name"> Last Name </label>
+                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"  required>
+              </div>
+              <div class="mb-4">
+                <label for="email">Email (optional)</label>
+                <input type="text" class="form-control" placeholder="you@example.com" aria-label="email">
+              </div>             
+              <div class="mb-4">
+                <label for="Address">Address</label>
+                <input type="text" class="form-control" placeholder="1234 Main St" aria-label="Address" id="shipping_address" name="shipping_address"  required>
               </div>
               
-              <div class="col mb-4">
-                <label for="Card2">
-                  Credit card Nummber
-                </label>
-                <input type="text" class="form-control" placeholder = "1234-5678-9012" aria-label="Card2" required>
+              <div class="mb-4">
+                <label for="Address2">Address 2 (optional)</label>
+                <input type="text" class="form-control" placeholder="Appartment or suite" aria-label="Address2">
               </div>
             </div>
-            
+          </div>
+          
+          <div class="container mb-4 p-4 glass-card">
             <div class="row">
-              <div class="col mb-3">
-                <label for="Card3">
-                  Expiry Date
-                </label>
-                <input type="text" class="form-control"aria-label="card3" required>
+              <div class="col">
+                <label for="country">Country</label>
+                <select class="form-select">
+                  <option selected>Choose...</option>
+                  <option value="1">Sri Lanka</option>
+                  <option value="2">Japan</option>
+                  <option value="3">Australlia</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="state">State</label>
+                <select class="form-select">
+                  <option selected>Choose...</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="col mb-4">
+                <label for="zip">Zip Code</label>
+                <input type="text" class="form-control" aria-label="zip">
               </div>
               
-              <div class="col mb-3">
-                <label for="Card4">
-                  CVV
+              <hr class="mb-4">
+              
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  Shipping address is the same as my billing address
                 </label>
-                <input type="text" class="form-control"  aria-label="Card4" required>
               </div>
+              
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                <label class="form-check-label" for="flexCheckChecked">
+                  Save this information for next time
+                </label>
+              </div>
+              
+              <hr class="mb-4">
+              
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Credit card (Default)
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                  Debit card
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
+                <label class="form-check-label" for="flexRadioDefault3">
+                  Paypal
+                </label>
+              </div>
+              
+              <div class="row">
+                <div class="col mb-4">
+                  <label for="Card1">
+                    Name on card
+                  </label>
+                  <input type="text" class="form-control"aria-label="card1" required>
+                  <small class="text-muted">
+                    Full name, as displayed on the card
+                  </small>
+                </div>
+                
+                <div class="col mb-4">
+                  <label for="Card2">
+                    Credit card Nummber
+                  </label>
+                  <input type="text" class="form-control" placeholder = "1234-5678-9012" aria-label="Card2" required>
+                </div>
+              </div>
+              
+              <div class="row">
+                <div class="col mb-3">
+                  <label for="Card3">
+                    Expiry Date
+                  </label>
+                  <input type="text" class="form-control"aria-label="card3" required>
+                </div>
+                
+                <div class="col mb-3">
+                  <label for="Card4">
+                    CVV
+                  </label>
+                  <input type="text" class="form-control"  aria-label="Card4" required>
+                </div>
+              </div> 
             </div>
-            
-          </div>
-        
-        <hr class="mb-4">
+          <hr class="mb-4">
+        </div>
         
         <div class="d-grid gap-2">
           <button class="btn btn-primary" type="submit">
