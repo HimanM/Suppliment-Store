50aa93e2cdf15b5c9f4d45f226ba06813ab76c02
HimanM <67066047+HimanM@users.noreply.github.com>
2024-10-01 03:08:57 +0530

    asda

--- access_denied.php
+++ access_denied.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="CSS/master.css">
-    <link rel="stylesheet" href="../CSS/error_styles.css">
 </head>
 <body>
 <?php include 'top_nav.php'; ?>
